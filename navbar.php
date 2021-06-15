ae54d156921151c287e8f2ea4aa710c190954f37
charayo <69734231+charayo@users.noreply.github.com>
2021-06-15 16:13:24 +0000

    recent update

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-light my-gold-txt">
+        <!--Brand-->
+        <a class="navbar-brand ml-3" href="index.html"><img src="images/logo.png" alt="logo" style="width: 50%; height: 50%;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <div id="collapsibleNavbar" class="navbar-collapse collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="services.html" class="nav-link">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="career.html" class="nav-link">Career</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact-us.html" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
